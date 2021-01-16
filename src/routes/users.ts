0691e9e0cebb8efd0415f916152d4752ad1c6205
uhziel <uhziel@gmail.com>
2021-01-16 21:32:59 +0800

    model对应的文件名统一改为驼峰命名法

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import User from '../models/user';
+import User from '../models/User';
 import {sign as signJWT} from 'jsonwebtoken';
 import {hashSync, compareSync} from 'bcrypt';
 
