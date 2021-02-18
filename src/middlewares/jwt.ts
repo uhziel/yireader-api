d50d98c0c80882a5c380b9a8d7bdbf6f23e8fa9b
uhziel <uhziel@gmail.com>
2021-02-18 16:56:16 +0800

    改变登录校验中间件 jwt 为异步版

--- src/middlewares/jwt.ts
+++ src/middlewares/jwt.ts
@@ -9,23 +9,29 @@ interface JWTPayload {
 }
 
 export const jwt = (req: Request, _res: Response, next: NextFunction) => {
-  const token = req.headers['authorization'];
-  if (!token) {
-    next(new Error('请先登录'));
-    return;
-  }
+  const p = new Promise((resolve, reject) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+      reject(new Error('请先登录'));
+      return;
+    }
 
-  try {
-    const payload = verifyJWT(token, SECRET_KEY) as JWTPayload;
-    req.user = {
-      id: payload.id,
-      username: payload.username,
-    };
-    next();
-  } catch (e) {
-    console.error(e);
-    next(e);
-  }
+    verifyJWT(token, SECRET_KEY, (e, data) => {
+      if (e) {
+        reject(e);
+        return;
+      }
+      const payload = data as JWTPayload;
+
+      req.user = {
+        id: payload.id,
+        username: payload.username,
+      };
+      resolve(null);
+    });
+  });
+
+  p.then(next, next);
 };
 
 export default jwt;
